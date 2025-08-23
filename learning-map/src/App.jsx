0979964dcde5ddd5ea5c0e0bb4481283307ce53c
Kundan Singh <kundansingh68788@gmail.com>
2025-08-23 10:31:22 +0530

    Changes commitetd

--- learning-map/src/App.jsx
+++ learning-map/src/App.jsx
@@ -2,9 +2,9 @@ import TodoItem from '../../todoapp-app/src/assets/ToDoItem';
 import './App.css'
 import List from './components/List';
 function App() {
-  const toDoItems=[{id: 1,toDoText: "Buy Milk " ,todoDate: "08-Aug-2025"}
-  ,{id: 2,toDoText: "Go to College" ,todoDate: "08-Aug-2025"}
-];
+//   const toDoItems=[{id: 1,toDoText: "Buy Milk " ,todoDate: "08-Aug-2025"}
+//   ,{id: 2,toDoText: "Go to College" ,todoDate: "08-Aug-2025"}
+// ];
 // function convertObjecttoTodoText(toDoItems){
 //   console.log(`Convert Function was called with item ${JSON.stringify(toDoItems)}, Converting this to ${toDoItems.toDoText}`);
 //   return toDoItems.toDoText;
@@ -13,14 +13,16 @@ function App() {
 //   const str=convertObjecttoTodoText(item);
 //   console.log(str);
 // })
-const todoText=toDoItems.map(item=>item.toDoText);
-console.log(todoText);
+// const todoText=toDoItems.map(item=>item.toDoText);
+// console.log(todoText);
 
  const arr=["Nirmala","Aman","Kundan","Abhijeet","Sadhana","Diptiman"];
+
     return (
        <>
       <div className='text-5xl'>Learners of Mern Stack </div>
      <List list={arr}/>
+
       </>       
   )
 }
--- learning-map/src/components/List.jsx
+++ learning-map/src/components/List.jsx
@@ -1,12 +1,16 @@
 const List=({list})=>{
+
+
 return (
 <>
-<ol className="list-decimal ml-10 text-xl">
+{(!list || list.length===0 )? <h1>No students are here</h1>
+                           :  <h1>Here are the students</h1>}
+{list && list.length>0 && <ol className="list-decimal ml-10 text-xl">
 {list.map(studentname=>
 <li>{studentname}</li>
 )}
 
-</ol>
+</ol>}
 <p>Without Using Map ........</p>
 <ol className="list-decimal ml-10 text-xl">
     {
